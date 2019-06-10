4eb8b5cb512c5137c991a3b8ee5e2128fca4b7b0
Daniel Frejek <daniel.frejek@stusta.net>
2019-06-10 18:54:07 +0200

    Added auto restart

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import weiche
+import machine
+
+time.sleep(1)
+
+
+while(True):
+    try:
+        import weiche
+    except KeyboardInterrupt:
+        # If we got a keyboard interrupt, we leave the loop.
+        break
+    except:
+        # Any other exception: Just restart the CPU
+        machine.reset()
+
