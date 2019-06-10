dcbb0d816ab945a2513c8f8d571dc1e7668a5973
Daniel Frejek <daniel.frejek@stusta.net>
2019-06-10 18:53:53 +0200

    Current running version of weiche

--- weiche.py
+++ weiche.py
@@ -3,20 +3,29 @@ from machine import Pin, unique_id, PWM
 import network
 import ubinascii
 import json
+import time
+import gc
+
 
 """
 all GPIOs have no internal PULL-UPs
 
-board  | chip   | led channel
--------+--------+---------------
-D1     | GPIO5  |     0
-D2     | GPIO4  |     1
-D5     | GPIO14 |     2
-D6     | GPIO12 |     3
+chip   | led channel
+-------+---------------
+GPIO4  |     0.0
+GPIO5  |     0.1
+GPIO12 |     1.0
+GPIO13 |     1.1
+GPIO14 |     2.0
+GPIO15 |     2.1
+GPIO0  |     3.0
+GPIO2  |     3.1
 """
 
-PWM_PINS = [5, 4, 14, 12]
+PWM_PINS = [4, 5, 12, 13, 14, 15, 0, 2]
 PWM_LEDS  = []
+
+statusled = Pin(16, Pin.OUT)
 
 CLIENT_ID = ubinascii.hexlify(unique_id())
 client_id = CLIENT_ID.decode('utf-8')
@@ -27,6 +36,7 @@ TOPIC = b"/haspa/led"
 
 # Received messages from subscriptions will be delivered to this callback
 def sub_cb(topic, msg):
+    global a
     try:
         msg = msg.decode('utf-8')
         j = json.loads(msg)
@@ -54,32 +64,49 @@ def sub_cb(topic, msg):
 
 
 def main(server=SERVER):
+    print("[*] waiting 0.5 seconds for good measure.".format(client_id))
     print("[*] weiche {} startet.".format(client_id))
     for pin in PWM_PINS:
         led = Pin(pin, Pin.OUT)
         PWM_LEDS.append(PWM(led))
 
+    statusled.on()
+
     # set pwm freq, its global for all pins
     PWM(led).freq(1000)
 
     for i, p in enumerate(PWM_LEDS):
-        if (i % 2) == 0:
+        if (i % 2) == 1:
             p.duty(512)
+        else:
+            p.duty(0)
+
 
     sta_if = network.WLAN(network.STA_IF)
     while not sta_if.isconnected():
-        print('Waiting for network connection...')
-        time.sleep(1)
+        print('[?] Waiting for network connection...')
+        time.sleep(0.5)
+        statusled.off()
+        time.sleep(0.5)
+        statusled.on()
 
+    print('[*] We have network')
     c = MQTTClient(CLIENT_ID, server)
     c.set_callback(sub_cb)
+    print("[*] connecting to mqtt")
     c.connect()
+    print("[*] connected")
     c.subscribe(TOPIC)
 
     c.publish(b"/haspa/power/status", b"{}")
 
+    statusled.off()
+    print("[*] Ready for messages")
     while True:
+        statusled.on()
         c.wait_msg()
+        statusled.off()
+        gc.collect()
 
     c.disconnect()
 
